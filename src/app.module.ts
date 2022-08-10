830812e13f1289d903cec31c89361ec2644ff12f
chenlibin <1113120212@qq.com>
2022-08-10 17:35:16 +0800

    feat:接入TypeOrm及mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
+/*
+ * @Author: chenlibin
+ * @Date: 2022-08-08 16:24:33
+ * @LastEditors: chenlibin
+ * @LastEditTime: 2022-08-09 09:24:06
+ * @FilePath: /src/app.module.ts
+ * @Description:
+ */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'clb961231',
+      database: 'test',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
